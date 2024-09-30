2930e3ecc5e6e4bf933374ad0f16e632001c06e1
MajorX234 <majorx234@googlemail.com>
2024-10-01 00:07:33 +0200

    Add example code to use regression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use regression::{
+    analytic::{self, Analytic},
+    model::LinearRegression,
+};
+
 fn main() {
-    println!("hello world");
+    let linear_regression = Analytic::new(vec![(0.0, 0.0), (1.0, 1.0)], 0.0, 0.0);
+    let (m, n) = linear_regression.step();
+    println!("m: {m}, n: {n}");
 }
